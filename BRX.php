61278c6bf3bde0f2b19bd5ac4b0304c26bfd5f7a
aluno <aluno@lab-wbs.local>
2016-03-30 08:28:11 -0300

    algoritmo par

---
+++ BRX.php
@@ -0,0 +1,23 @@
+<html>
+	<head>
+		<title>imagem de fundo</TITLE>
+	</head>
+
+	<body>
+	
+		</body>
+ </html>
+		<?php
+			
+
+			
+			
+			for ($i = 0; $i <= 100; $i++) {
+			if(($i % 2) ==0) {
+			
+            echo"<br/>Numeros par: " .$i;
+			}
+			
+			}
+			?>
+
