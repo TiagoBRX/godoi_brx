b52a53a83510ac99b6b6b29764b2a16ceca09ed2
Aluno <Aluno@labwbs02>
2016-08-11 08:50:26 -0300

    mecher na pasta do salvar

--- projeto-feira/inscricao/salvar.php
+++ projeto-feira/inscricao/salvar.php
@@ -2,4 +2,17 @@
 	echo "GET:" . var_dump($_GET) . "<br>";
 	echo "POST:" . var_dump($_POST) . "<br>";
 	echo "posicao nome" . $_POST['nome'];
-?>
+   
+	/*configucao de endereçamento do banco de dados*/
+	$servidor = "localhost";
+	$usuario = "root";
+	$senha = "";
+	
+	/*configuraçao de acesso o banco de dados*/
+	$nome_banco = "bd_centro_interessee";
+	
+	$conexao = mysql_connect($servidor, $usuario, $senha);
+	
+	$banco = mysql_select_db($nome_banco, $conexao);
+	
+	?>
