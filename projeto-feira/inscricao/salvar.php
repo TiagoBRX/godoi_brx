c43c03ce8ddb405725551904536a9cdf2d198926
Aluno <Aluno@labwbs02>
2016-08-17 09:43:20 -0300

    alteraçao nos codigos das pasta salvar.php,incricao.html

--- projeto-feira/inscricao/salvar.php
+++ projeto-feira/inscricao/salvar.php
@@ -2,17 +2,30 @@
 	echo "GET:" . var_dump($_GET) . "<br>";
 	echo "POST:" . var_dump($_POST) . "<br>";
 	echo "posicao nome" . $_POST['nome'];
-   
-	/*configucao de endereçamento do banco de dados*/
-	$servidor = "localhost";
-	$usuario = "root";
-	$senha = "";
 	
-	/*configuraçao de acesso o banco de dados*/
-	$nome_banco = "bd_centro_interessee";
+	
+	
+	/*configuraçao de endereçamento do banco de dados*/
+	$servidor="localhost";
+	$usuario ="root";
+	$senha ="";
+	
+	/*configuraçao do banco de dados*/
+	$nome_banco= "bd_centro_interessee";
 	
 	$conexao = mysql_connect($servidor, $usuario, $senha);
 	
-	$banco = mysql_select_db($nome_banco, $conexao);
+	$banco = mysql_select_db($nome_banco,$conexao);
 	
-	?>
+	/*verificar se a conexao realmente foi criada*/
+	/*se (nao conexao) entao, ou seja,  conexao e falsa*/
+	if (!$conexao) {
+		echo "nao foi posivel conectar ao banco de dados mysql.";
+		exit;
+	}else{/*senao*/
+	echo " conectou!";
+	}
+	
+	/*selecione de acesso o banco de dados*/
+	
+?>
